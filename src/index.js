8d41ce498270f4ec097be584a831325c9ed12d5c
sharon-yelenik <sharon.yelenik@cloudinary.com>
2025-01-19 15:07:34 +0200

    from template

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import "./styles.css";
-import "./index.js";
+import './styles.css';
 
 window.ml = cloudinary.createMediaLibrary(
   {
-    cloud_name: "YOUR_CLOUD_NAME",
-    api_key: "YOUR_API_KEY",
+    cloud_name: 'yelenik',
+    api_key: '428712716519232',
     remove_header: false,
-    max_files: "1",
-    insert_caption: "Insert",
-    inline_container: "#widget_container",
+    max_files: '1',
+    insert_caption: 'Insert',
+    inline_container: '#widget_container',
     default_transformations: [[]],
-    button_class: "myBtn",
-    button_caption: "Select Image or Video"
+    button_class: 'myBtn',
+    button_caption: 'Select Image or Video',
   },
   {
     insertHandler: function (data) {
       data.assets.forEach((asset) => {
-        console.log("Inserted asset:", JSON.stringify(asset, null, 2));
+        console.log('Inserted asset:', JSON.stringify(asset, null, 2));
       });
-    }
+    },
   },
-  document.getElementById("open-btn")
+  document.getElementById('open-btn')
 );
